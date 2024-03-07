948b59ba39f407bb3149ccce12be7d4553913309
Tomoya Koyanagi <tomk79@gmail.com>
2024-03-08 00:15:07 +0900

    add: front end assets

---
+++ webpack.mix.js
@@ -0,0 +1,39 @@
+const mix = require('laravel-mix');
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel applications. By default, we are compiling the CSS
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+	.webpackConfig({
+		module: {
+			rules:[
+				{
+					test: /\.txt$/i,
+					use: ['raw-loader'],
+				}
+			]
+		},
+		resolve: {
+			fallback: {
+				"fs": false,
+				"path": false,
+				"crypto": false,
+				"stream": false,
+			}
+		}
+	})
+
+
+	// --------------------------------------
+	// px2-site-search.js
+	.js('src/px2-site-search.js', 'public/')
+	.sass('src/px2-site-search.scss', 'public/')
+;
