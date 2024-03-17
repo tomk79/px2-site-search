a6e4930c9fca062e095c672dc14e71dfb01780cb
Tomoya Koyanagi <tomk79@gmail.com>
2024-03-17 15:02:35 +0900

    CCE: 実行中のボタン制御を追加

--- cce/src/siteSearchCceFront.js
+++ cce/src/siteSearchCceFront.js
@@ -1,22 +1,28 @@
 window.siteSearchCceFront = function(cceAgent){
-    let $elm = cceAgent.elm();
+	let $elm = cceAgent.elm();
 
+	$elm.innerHTML = `
+		<p>インデックスを更新します。</p>
+		<p><button type="button" class="px2-btn px2-btn--primary cont-btn-create-index">インデックスを更新</button></p>
+	`;
 
-    $elm.innerHTML = `
-        <p>インデックスを更新します。</p>
-        <p><button type="button" class="px2-btn px2-btn--primary cont-btn-create-index">インデックスを更新</button></p>
-    `;
+	$elm.querySelector('button')
+		.addEventListener('click', function(){
+			const elm = this;
+			px2style.loading();
+			elm.setAttribute('disabled', true);
 
-    $elm.querySelector('button').addEventListener('click', function(){
-        cceAgent.gpi({
-            'command': 'create_index'
-        }, function(res){
-            console.log('---- res:', res);
-            if(res.result){
-                alert('インデックスを更新しました。');
-            }else{
-                alert('[ERROR] インデックスの更新に失敗しました。');
-            }
-        });
-    });
+			cceAgent.gpi({
+				'command': 'create_index'
+			}, function(res){
+				console.log('---- res:', res);
+				if(res.result){
+					alert('インデックスを更新しました。');
+				}else{
+					alert('[ERROR] インデックスの更新に失敗しました。');
+				}
+				px2style.closeLoading();
+				elm.removeAttribute('disabled');
+			});
+		});
 }
